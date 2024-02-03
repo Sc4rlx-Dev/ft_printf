79a3b732fdb229999cd171bbd433fe41d48eaaf8
scarlx <101978376+Sc4rlx-Dev@users.noreply.github.com>
2024-02-03 19:01:36 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -15,9 +15,11 @@ int		ft_print_unsigned(unsigned int n);
 int		ft_print_hex(unsigned int num, const char format);
 int		ft_printpercent(void);
 void	ft_putstr(char *str);
-char	*ft_uitoa(unsigned int n);
 int		ft_num_len(unsigned	int num);
 void	ft_put_hex(unsigned int num, const char format);
 int		ft_hex_len(unsigned	int num);
+char	*ft_uitoa(unsigned int n);
+char	*ft_itoa(int n);
+
 
 #endif
