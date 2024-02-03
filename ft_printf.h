0c905f0f76bb5dff0cad838b642939cc3f3319fc
scarlx <101978376+Sc4rlx-Dev@users.noreply.github.com>
2024-02-03 19:00:07 +0100

    Add files via upload

---
+++ ft_printf.h
@@ -0,0 +1,23 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdint.h>
+
+int		ft_printf(const char *format, ...);
+int		ft_formats(va_list args, const char format);
+int		ft_printchar(int c);
+int		ft_printstr(char *str);
+int		ft_print_ptr(unsigned long long ptr);
+int		ft_printnbr(int n);
+int		ft_print_unsigned(unsigned int n);
+int		ft_print_hex(unsigned int num, const char format);
+int		ft_printpercent(void);
+void	ft_putstr(char *str);
+char	*ft_uitoa(unsigned int n);
+int		ft_num_len(unsigned	int num);
+void	ft_put_hex(unsigned int num, const char format);
+int		ft_hex_len(unsigned	int num);
+
+#endif
---
+++ ft_utilsV2.c
@@ -0,0 +1,46 @@
+static int	how_many_to_alloc(int n)
+{
+	int	i;
+	int	tmp;
+
+	i = 0;
+	tmp = n;
+	if (n == 0)
+		return (1);
+	while (n)
+	{
+		i++;
+		n /= 10;
+	}
+	if (tmp > 0)
+		return (i);
+	return (i + 1);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+	long	tmp;
+
+	len = how_many_to_alloc(n);
+	tmp = (long)n;
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (str);
+	if (tmp < 0)
+	{
+		tmp = -tmp;
+		str[0] = '-';
+	}
+	str[len--] = '\0';
+	if (tmp == 0)
+		str[0] = '0';
+	while (tmp)
+	{
+		str[len] = tmp % 10 + '0';
+		len--;
+		tmp /= 10;
+	}
+	return (str);
+}
