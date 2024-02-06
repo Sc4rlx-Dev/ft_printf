a22653f949bd862d33e598e5eda230db162a9e41
scarlx <101978376+Sc4rlx-Dev@users.noreply.github.com>
2024-02-06 12:31:02 +0100

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,15 +1,14 @@
 CC = cc
-CFLAGS = -Wall -Wextra -Werror 
+CFLAGS = -Wall -Wextra -Werror
 RM = rm -rf
 NAME = libftprintf.a
 
-SRCS = ft_print_hex.c ft_print_ptr.c ft_print_utils.c ft_print_utilsV2.c ft_print.c
+SRCS = ft_print_ptr.c ft_printf_utils.c ft_utilsV2.c ft_print_hex.c ft_printf.c
 OBJS = $(SRCS:.c=.o)
 
 all: $(NAME)
 
 $(NAME): $(OBJS)
-	$(CC) $(CFLAGS) -c $^
 	ar rc $(NAME) $(OBJS)
 
 %.o: %.c
@@ -17,7 +16,7 @@ $(NAME): $(OBJS)
 
 clean:
 	$(RM) $(OBJS)
-
+	
 fclean: clean
 	$(RM) $(NAME)
 
--- ft_printf.h
+++ ft_printf.h
@@ -1,9 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oer-raou <oer-raou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 12:29:55 by oer-raou          #+#    #+#             */
+/*   Updated: 2024/02/06 12:29:56 by oer-raou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stdarg.h>
-# include <unistd.h>
-# include <stdint.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>  
+#include <unistd.h>
+
 
 int		ft_printf(const char *format, ...);
 int		ft_formats(va_list args, const char format);
@@ -17,8 +33,10 @@ int		ft_printpercent(void);
 void	ft_putstr(char *str);
 char	*ft_uitoa(unsigned int n);
 int		ft_num_len(unsigned	int num);
+char	*ft_itoa(int n);
 void	ft_put_hex(unsigned int num, const char format);
 int		ft_hex_len(unsigned	int num);
-void	ft_putchar_fd(char c, int fd)
+void	ft_putchar_fd(char c, int fd);
+
 
 #endif
